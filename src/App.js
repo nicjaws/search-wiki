61bda68f36de7817ef7691ad636c8dfe66caa1aa
Nicolas Corbalan <outreef@hotmail.com>
2020-11-18 19:13:36 +0100

    add the first custom hook

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import ReactAutocomplete from 'react-autocomplete';
-import axios from 'axios';
+import {useSearch} from './hooks';
 
 function App() {
   const [value, setValue] = useState('');
-  const [items, setItems] = useState([]);
+  const {articles} = useSearch(value);
 
-  useEffect( () => {
-    axios.get(`https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${value}`)
-      .then(function (response) {
-        const parsedResponse = [];
-
-        for (let i = 0; i < response.data[1].length; i++){
-          parsedResponse.push({
-            id: response.data[3][i],
-            label: response.data[1][i]
-          })
-        }
-        setItems(parsedResponse)
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-        debugger
-      })
-  }, [value])
-  
   return (
     <ReactAutocomplete
-        items={items}
+        items={articles}
         shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
         getItemValue={item => item.label}
         renderItem={(item, highlighted) =>
